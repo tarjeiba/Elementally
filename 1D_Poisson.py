f3f13e1fbf7f59ea607b73932c9884f4d7cf5f59
Tarjei Bærland <tarjeibaerland@gmail.com>
2014-06-28 19:57:46 +0200

    Both the stiffness matrix and the loading vector is now defined for Poisson 1D.

--- 1D_Poisson.py
+++ 1D_Poisson.py
@@ -5,7 +5,7 @@
 #
 
 import numpy as np
-from Integrator import gaussian
+from Integrators import gaussian as gauss
 import matplotlib as matplot
 
 # Initiating characteristics for a uniform 1D mesh
@@ -14,15 +14,21 @@ x_N = 10
 num_elements = 10
 nodes = np.linspace(x_0, x_N, num_elements + 1)
 h = nodes[1] - nodes[0]
+f = lambda x: x
+
+# Setting up the boundary conditions
+
+
+
 
 # ASSEMBLY OF STIFFNESS MATRIX:
 # Initialise A-matrix:
 A = np.zeros((len(nodes), len(nodes)))
 for i in range(num_elements):
-    A[i,i] = A[i,i] + 1./h
-    A[i,i+1] = A[i,i+1] - 1./h
-    A[i+1,i] = A[i+1,i] - 1./h
-    A[i+1,i+1] = A[i+1,i+1] + 1./h
+    A[i,i] += 1./h
+    A[i,i+1] += -1./h
+    A[i+1,i] += -1./h
+    A[i+1,i+1] += 1./h
 
 print "Nodes: "
 print nodes
@@ -32,4 +38,14 @@ print A
 # ASSEMBLY OF LOADING VECTOR
 
 b = np.transpose(np.zeros(len(nodes)))
+for i in range(num_elements):
+    b[i] += gauss.gaussian_quad_1d(nodes[i], nodes[i+1],
+                                   lambda x: ( nodes[i+1] - x ) / h * f(x),
+                                   1)
+    b[i+1] += gauss.gaussian_quad_1d(nodes[i], nodes[i+1],
+                                   lambda x: ( x - nodes[i] ) / h * f(x),
+                                   1)
+print "Loading vector:"
+print b
+    
 
