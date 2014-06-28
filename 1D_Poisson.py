46c8329dd92f14f31e41ab24651614afd9f9f54f
Trygve Bærland <trygve.baerland@gmail.com>
2014-06-28 22:06:01 +0200

    Per now, does not give correct solution.

--- 1D_Poisson.py
+++ 1D_Poisson.py
@@ -7,6 +7,7 @@
 import numpy as np
 from Integrators import gaussian as gauss
 import matplotlib as matplot
+import matplotlib.pyplot as pyplot
 
 # Initiating characteristics for a uniform 1D mesh
 x_0 = 0
@@ -15,12 +16,12 @@ num_elements = 10
 nodes = np.linspace(x_0, x_N, num_elements + 1)
 h = nodes[1] - nodes[0]
 f = lambda x: x
-
+print nodes
 # Setting up the boundary conditions
 # Dirichlet on the left end point:
-u_0 = 5
+u_0 = 1
 # Neumann on the right end point:
-du_end = 1
+du_end = -1
 
 
 # ASSEMBLY OF STIFFNESS MATRIX:
@@ -32,21 +33,16 @@ for i in range(num_elements):
     A[i+1,i] += -1./h
     A[i+1,i+1] += 1./h
 
-print "Nodes: "
-print nodes
-print "Stiffness matrix: "
-print A
-
 # ASSEMBLY OF LOADING VECTOR
 
 b = np.transpose(np.zeros(len(nodes)))
 for i in range(num_elements):
     b[i] += gauss.gaussian_quad_1d(nodes[i], nodes[i+1],
                                    lambda x: ( nodes[i+1] - x ) / h * f(x),
-                                   1)
+                                   4)
     b[i+1] += gauss.gaussian_quad_1d(nodes[i], nodes[i+1],
                                    lambda x: ( x - nodes[i] ) / h * f(x),
-                                   1)
+                                   4)
 print "Loading vector:"
 print b
 
@@ -56,6 +52,12 @@ A[0,0] = 1
 b[0] = u_0 
 # ADDING CONTRIBUTION FROM NEUMANN CONDITION TO LOADING VECTOR:
 b[-1] += du_end
-
+print b
 # SOLVING RESULTING SYSTEM:
 u = np.linalg.solve(A,b)
+
+# PLOTTING SOLUTION
+pyplot.plot(nodes,u,'b')
+u_exact = -1./6 * nodes ** 3 + 4.8*nodes + 1
+pyplot.plot(nodes,u_exact,'r')
+pyplot.show()
