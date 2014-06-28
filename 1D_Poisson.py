2eb92b7364b88e36b7fcafba413d954b06988638
Tarjei Bærland <tarjeibaerland@gmail.com>
2014-06-28 17:08:16 +0200

    Moved the 1D_Poisson script.

--- 1D_Poisson.py
+++ 1D_Poisson.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python
 #
 # This is an initial attempt of an 1D Poisson solver
 # written by Trygve and Tarjei, June 26 2014
 #
 
 import numpy as np
+from Integrator import gaussian
 import matplotlib as matplot
 
 # Initiating characteristics for a uniform 1D mesh
@@ -11,11 +13,11 @@ x_0 = 0
 x_N = 10
 num_elements = 10
 nodes = np.linspace(x_0, x_N, num_elements + 1)
-h = x[1] - x[0]
+h = nodes[1] - nodes[0]
 
-#ASSEMBLY OF STIFFNESS MATRIX:
-#Initialise A-matrix:
-A = np.zeros((len(nodes), len(nodes))
+# ASSEMBLY OF STIFFNESS MATRIX:
+# Initialise A-matrix:
+A = np.zeros((len(nodes), len(nodes)))
 for i in range(num_elements):
     A[i,i] = A[i,i] + 1./h
     A[i,i+1] = A[i,i+1] - 1./h
@@ -26,3 +28,8 @@ print "Nodes: "
 print nodes
 print "Stiffness matrix: "
 print A
+
+# ASSEMBLY OF LOADING VECTOR
+
+b = np.transpose(np.zeros(len(nodes)))
+
