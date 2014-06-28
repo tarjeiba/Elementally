437d9bb9d9ca46f773d8f347df762f20857ae9cc
Tarjei Bærland <tarjeibaerland@gmail.com>
2014-06-28 14:18:55 +0200

    Directory for numerical integrators.

--- 1D_Poisson/1D_Poisson.py
+++ 1D_Poisson/1D_Poisson.py
@@ -5,4 +5,11 @@
 
 import numpy as np
 import matplotlib as matplot
-print "Hello"
+
+# Initiating charactestics for a uniform 1D mesh
+x_0 = 0
+x_N = 10
+num_nodes = 10
+nodes = np.linspace(x_0, x_N, num_nodes)
+
+print nodes
---
+++ Integrators/gaussian.py
@@ -0,0 +1,12 @@
+# This is the module file for the gaussian integrator
+# Created June 26 2014
+
+def gaussian_quad_1d():
+    """
+    One-dimensional integrator using Gaussian quadrature.
+    Typical usage:
+
+
+    """
+
+    pass
