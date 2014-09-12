ad4e8391737abcf7f204790ee486fe606598aa4f
Trygve Bærland <trygve.baerland@gmail.com>
2014-09-12 18:13:26 +0200

    ...

--- 2d_poisson.py
+++ 2d_poisson.py
@@ -6,23 +6,43 @@
 
 import numpy as np
 from Integrators import gaussian as gauss
+from Meshers import mesh_kit_2D as mesh_kit
 import matplotlib.pyplot as plt
 
 ###########################
-#SET UP PARAMETERS:
+# SET UP PARAMETERS:
 ##########################
 
+# Poisson function:
+def f(x):
+    return -1.
+
+# Dirichlet function:
+def g(x):
+    return 0.
+
+# Neumann function:
+def h(x):
+    return 1.
 
 ##########################
-#GENERATE MESH DOMAIN:
+# GENERATE MESH DOMAIN:
 ##########################
 
+mesh = mesh_kit.quarter_annulus_2D( 0.1 )
+
+points = np.array(mesh.points)
+print np.array(mesh.facets)
+for face in np.array(mesh.facets):
+    print face, mesh.points[face[0]], mesh.points[face[1]]
+
+plt.triplot(points[:,0], points[:,1],np.array(mesh.elements))
+plt.show()
 ##########################
 # ASSEMBLY:
 ##########################
 
-#Needs: Line integral (for loading vector).
-#   2D quadrature for stiffness matrix and load vector.
+#Needs: 
 #   Our understanding of the data structure.
 
 ##########################
