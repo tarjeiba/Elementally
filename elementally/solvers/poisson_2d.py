71e9478183ce3f80c7a1bd21d0fb640f57295e55
Trygve Bærland <trygve.baerland@gmail.com>
2014-12-23 19:10:29 +0100

    Removed a superfluous print statement.

--- elementally/solvers/poisson_2d.py
+++ elementally/solvers/poisson_2d.py
@@ -102,8 +102,6 @@ print "Solving linear system: ", time.time() - time1
 fig1 = plt.figure(1)
 ax = fig1.gca(projection='3d')
 
-surf = ax.plot_trisurf(points[:,0], points[:,1], u, triangles = mesh.elements, cmap=cm.jet, linewidth=0.2)
-print surf.__dict__
 plt.show(1)
 
 #print mesh.facets
